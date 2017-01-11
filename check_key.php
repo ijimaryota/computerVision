520699008de6bc86f875218dd82a38049e854d5d
Ryota_ijima <k12008kk@aitech.ac.jp>
2017-01-11 17:39:14 +0900

    php上でtrue,falseを判別するように変更

--- check_key.php
+++ check_key.php
@@ -1,19 +1,29 @@
 <?php
-  echo '<pre>';
-  $image_path = 'img/lock.png';
   // シェルコマンド "ls" の全ての結果を出力し、出力の最後の
   // 行を $last_line に格納します。シェルコマンドの戻り値は
   // $retval に格納されます。
-  $last_line = system('ls', $retval);
+  // $img_url1='koyama/bg.jpg';
+  // $img_url2='koyama/face.jpg';
+  //
+  // $test = system('koyama/tempTest,koyama/bg.png,koyama/face.jpg',$img_t_f);
+  // echo $img_t_f;
+  // echo $test;
+
+  echo exec('koyama/tempTest,koyama/bg.png,koyama/face.jpg');
 
   // 追加情報を表示します。
-  if($retval){
+  if(exec('koyama/tempTest,koyama/bg.png,koyama/face.jpg')){
+    // echo $img_t_f;
+    // echo $test;
     echo '<hr />key is locked   ';
-
+    echo '<img src="img/lock.png" width="300" height="300">';
 
 
   }else{
+    // echo $img_t_f;
+    // echo $test;
     echo '<hr />key is Unlocked';
+    echo '<img src="img/unlock.png" width="350" height="300">';
   }
 ?>
 
@@ -24,8 +34,8 @@
 </HEAD>
 <BODY>
 
-<img src="img/lock.png" width="300" height="300">
-<img src="img/unlock.png" width="300" height="300">
+<!-- <img src="img/lock.png" width="300" height="300">
+<img src="img/unlock.png" width="300" height="300"> -->
 
 </BODY>
 </HTML>
