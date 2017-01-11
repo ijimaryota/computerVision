dd0c0790dc7a0ecf62fab0eba1453505dfcb9cf2
Ryota_ijima <k12008kk@aitech.ac.jp>
2017-01-11 17:06:09 +0900

    commit

--- check_key.php
+++ check_key.php
@@ -1,6 +1,6 @@
 <?php
   echo '<pre>';
-
+  $image_path = 'img/lock.png';
   // シェルコマンド "ls" の全ての結果を出力し、出力の最後の
   // 行を $last_line に格納します。シェルコマンドの戻り値は
   // $retval に格納されます。
@@ -8,10 +8,24 @@
 
   // 追加情報を表示します。
   if($retval){
-    echo '
-    <hr />key is locked   ';
+    echo '<hr />key is locked   ';
+
+
+
   }else{
-    echo '
-    <hr />key is Unlocked';
+    echo '<hr />key is Unlocked';
   }
 ?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE>サンプルページ</TITLE>
+</HEAD>
+<BODY>
+
+<img src="img/lock.png" width="300" height="300">
+<img src="img/unlock.png" width="300" height="300">
+
+</BODY>
+</HTML>
